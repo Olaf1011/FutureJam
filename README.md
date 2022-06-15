54cd0e66a9f6cd2ba5d3980b530a28f6c0536b76
olaf oude reimer <orolaf@live.nl>
2022-06-15 13:28:37 +0100

    Test

---
+++ README.md
@@ -0,0 +1,3 @@
+# FutureJaming
+
+Developed with Unreal Engine 5
---
+++ Source/FutureJaming/FutureJamingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FutureJamingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FUTUREJAMING_API AFutureJamingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
